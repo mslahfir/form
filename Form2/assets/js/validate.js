4c171295580054acfd14f7c45dc9c0e5e4014718
19i234 <19i234@IT-CIRRUS.COM>
2022-03-04 11:03:15 +0530

    sads

--- Form2/assets/js/validate.js
+++ Form2/assets/js/validate.js
@@ -1,10 +1,8 @@
-$(document).ready(function () {
- 
-});
+$(document).ready(function () {});
 $("#form").submit(function (e) {
-    e.preventDefault();
-    var regex = /^[a-zA-Z ]*$/;
-    const form = $("#form");
-    const formData = new FormData(form);
-    console.log(formData.get("email"));
-  });
+  e.preventDefault();
+  var regex = /^[a-zA-Z ]*$/;
+  const form = $("#form");
+  const formData = new FormData(form);
+  console.log(formData.get("email"));
+});
