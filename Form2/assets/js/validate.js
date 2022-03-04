a391e12f713ddf33b747f1972113d14878c383c5
19i234 <19i234@IT-CIRRUS.COM>
2022-03-04 10:40:17 +0530

    form3

--- Form2/assets/js/validate.js
+++ Form2/assets/js/validate.js
@@ -1,7 +1,10 @@
-$( document ).ready(function() {
-    $("#form").submit(function(e){
-        e.preventDefault();
-        const formData = new FormData($("#form"));
-        console.log(formData.get("email"))
-    })
+$(document).ready(function () {
+ 
 });
+$("#form").submit(function (e) {
+    e.preventDefault();
+    var regex = /^[a-zA-Z ]*$/;
+    const form = $("#form");
+    const formData = new FormData(form);
+    console.log(formData.get("email"));
+  });
