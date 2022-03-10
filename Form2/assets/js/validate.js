1aa963981bf8d81daf29c830661c1b2548b5431d
Lahfir <70215676+lahfir@users.noreply.github.com>
2022-03-10 20:31:19 +0530

    all

--- Form2/assets/js/validate.js
+++ Form2/assets/js/validate.js
@@ -1,8 +1,18 @@
-$(document).ready(function () {});
-$("#form").submit(function (e) {
-  e.preventDefault();
-  var regex = /^[a-zA-Z ]*$/;
-  const form = $("#form");
+function validate(e) {
+  const form = document.querySelector("#form");
   const formData = new FormData(form);
-  console.log(formData.get("email"));
-});
+
+  e.preventDefault();
+
+  const Nameregex = /^[a-zA-Z]+$/;
+  const Phoneregex = /^\d+$/;
+  if (!Nameregex.test(formData.get("empName"))) {
+    alert("Employee name should contain only alphabetic characters");
+  } else if (!Phoneregex.test(formData.get("Ph No"))) {
+    alert("Employee Phone number should contain only numbers");
+  } else {
+    alert("Successfully Registered");
+  }
+
+  console.log(empName);
+}
